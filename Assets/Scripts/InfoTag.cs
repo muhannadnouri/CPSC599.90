8cdefed8a3cc20ab25abde2b1613ab494576d285
Brandon G <brandongob@gmail.com>
2019-03-19 22:57:58 -0600

    added info tag

---
+++ Assets/Scripts/InfoTag.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoTag : MonoBehaviour
+{
+    public string MarkerName;
+    public GameObject ObjectToToggleVisibility;
+    private Renderer _textRenderer;
+    private bool _isMarkerShowing;
+    void Start()
+    {
+        _isMarkerShowing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkForTouch();
+        checkForMouseClick();
+        toggleInfoMarkerVisibility();
+    }
+    private void checkForTouch()
+    {
+        if (Input.touches.Length > 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                _isMarkerShowing = !_isMarkerShowing;
+            }
+        }
+    }
+    private void checkForMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                _isMarkerShowing = !_isMarkerShowing;
+            }
+        }
+    }
+    private void toggleInfoMarkerVisibility()
+    {
+        ObjectToToggleVisibility.SetActive(_isMarkerShowing);
+    }
+}
