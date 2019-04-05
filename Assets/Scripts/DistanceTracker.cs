46f14af4a7ac588269e3ed7a1e590f3e8da4f0e7
eunwolie <eve.chen@rocketmail.com>
2019-04-05 01:46:27 -0600

    Map Added
    Wrong repo added previously

---
+++ Assets/Scripts/DistanceTracker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DistanceTracker : MonoBehaviour
+{
+    Animator anim;
+
+    TrackableBehaviour myTrackableBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = Camera.main.transform.position - myTrackableBehaviour.transform.position;
+        float distance = delta.magnitude;
+
+        if (distance > 0)
+        {
+            anim.SetTrigger("Resting");
+            anim.SetTrigger("IsClose");
+        } else if (distance > 5)
+        {
+            anim.ResetTrigger("Resting");
+        }
+    }
+}
---
+++ Assets/Scripts/Trackable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Trackable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TrackableBehaviour tb = this.GetComponentInChildren<TrackableBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
