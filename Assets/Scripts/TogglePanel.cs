626ca9079cf78ea8987d1fcc88562ecda80bd12d
Eve <eve.chen@rocketmail.com>
2019-04-08 12:09:12 -0600

    Map Function Updated
    New Image needed

---
+++ Assets/Scripts/TogglePanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePanel : MonoBehaviour
+{
+
+    public void Toggle()
+    {
+            gameObject.SetActive(!gameObject.activeSelf);
+        
+    }
+
+}
